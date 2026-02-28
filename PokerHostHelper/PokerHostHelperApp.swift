ee04fd5d630a5babd3b09c14165847af7e34d5e8
Alex Hakimzadeh <alexhakimzadeh@gmail.com>
2026-02-27 21:57:14 -0600

    Initial Commit

---
+++ PokerHostHelper/PokerHostHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerHostHelperApp.swift
+//  PokerHostHelper
+//
+//  Created by Alex Hakimzadeh on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PokerHostHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
